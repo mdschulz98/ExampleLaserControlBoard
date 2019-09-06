3b7be24315b19217e256f56c43a72110642e875c
mdschulz98 <lestrev@gmail.com>
2019-09-06 14:57:34 -0600

    Add project files.

---
+++ ExampleLaserControlBoard/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OEI.LaserControlBoard;
+
+namespace ExampleLaserControlBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LaserControlBoard lcb = new LaserControlBoard();
+            lcb.OpenCommunication(9);
+            LaserControlGlobalState laserControlGlobalState = lcb.GetGlobalState();
+            lcb.SetInterlock(true);
+            Console.WriteLine(laserControlGlobalState.Interlock);
+        }
+    }
+}
